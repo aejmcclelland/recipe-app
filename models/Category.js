7b234a4b8b3ef287da9a2a8ce94f8e6deb76ae83
aejmcclelland <aejmcclelland@googlemail.com>
2024-11-25 22:46:20 +0000

    corrected the category model

--- models/Category.js
+++ models/Category.js
@@ -1,13 +1,13 @@
 import pkg from 'mongoose'; // Import the entire mongoose package
 const { Schema, model, models } = pkg;
 
-const categorySchema = new Schema({
+const CategorySchema = new Schema({
 	name: {
 		type: String,
 		required: true, // Example: 'Beef', 'Chicken', 'Pasta'
 	},
 });
 
-const Category = models.Category || model('Category', categorySchema);
+const Category = models.Category || model('Category', CategorySchema);
 
 export default Category;
