f9ad9c607b5f15cf66bffc2bd7f2e58f20d11db5
aejmcclelland <aejmcclelland@googlemail.com>
2025-05-19 08:28:18 +0100

    corrected redirect in recipes/page.jsx

--- app/recipes/page.jsx
+++ app/recipes/page.jsx
@@ -7,28 +7,26 @@ import RecipeSearchForm from '@/components/RecipeSearchForm';
 import BackToHomeButton from '@/components/BackToHomeButton';
 import RecipesClient from '@/components/RecipesClient';
 import { getSessionUser } from '@/utils/getSessionUser';
-
+import { redirect } from 'next/navigation';
 
 export const dynamic = 'force-dynamic';
 
 export default async function RecipesPage() {
+    const sessionUser = await getSessionUser();
+    if (!sessionUser) {
+        redirect('/recipes/signin');
+    }
+    const user = sessionUser?.user || null;
     try {
         await connectDB();
 
         const recipes = await Recipe.find().populate('category').lean();
         const recipesWithIds = convertToSerializeableObject(recipes);
 
-        console.log('Serialized recipes:', recipesWithIds);
-
         // Fetch user data
         // Fetch user session data
-        const sessionUser = await getSessionUser();
-        if (!sessionUser) {
-            console.warn('No session user found');
-        }
-        const user = sessionUser?.user || null;
 
-        console.log('User passed to RecipeOverviewCard:', user);
+
 
         return (
             <Container>
