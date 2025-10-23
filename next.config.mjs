b08f2183b979cc90980a22cf600fa9d02fee066b
aejmcclelland <aejmcclelland@googlemail.com>
2025-10-23 17:11:33 +0100

    removed i18n in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,46 +1,35 @@
 import 'dotenv/config';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-
-	env: {
-		NEXTAUTH_URL: process.env.VERCEL_URL
-			? `https://${process.env.VERCEL_URL}` // Use Vercel URL in production
-			: 'http://localhost:3000', // Use localhost in development
-		MONGODB_URI: process.env.MONGODB_URI,
-	},
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'res.cloudinary.com', // Cloudinary hostname
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'images.immediate.co.uk', // BBC Good Food images
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'cdn.sanity.io', // ✅ Add this
-				pathname: '/**',
-			},
-		],
-	},
-	async redirects() {
-		return [
-			{
-				source: '/recipes/privacy-policy',
-				destination: '/privacy-policy',
-				permanent: true,
-			},
-		];
-	},
-	i18n: {
-		locales: ['en'],
-		defaultLocale: 'en',
-	},
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com', // Cloudinary hostname
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.immediate.co.uk', // BBC Good Food images
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io', // ✅ Add this
+        pathname: '/**',
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/recipes/privacy-policy',
+        destination: '/privacy-policy',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
