beaead0bd826c7e91b3805ec78120ddae7106eb5
aejmcclelland <aejmcclelland@googlemail.com>
2025-12-06 20:28:21 +0000

    refactored auth route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,14 +1,7 @@
-// import NextAuth from 'next-auth';
-// import { authOptions } from '@/utils/authOptions';
+import NextAuth from 'next-auth';
+import { authOptions } from '@/utils/authOptions';
 
-// const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
-// export { handler as GET, handler as POST };
+export { handler as GET, handler as POST };
 
-export function GET() {
-	return new Response('ok from auth GET');
-}
-
-export function POST() {
-	return new Response('ok from auth POST');
-}
